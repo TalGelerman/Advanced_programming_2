2167434dc74d66e38b045c687b2699117c0a1a51
TalGelerman <106587324+TalGelerman@users.noreply.github.com>
2023-01-02 19:50:22 +0200

    gitignore

--- Client.h
+++ Client.h
@@ -1,4 +1,18 @@
 #ifndef MAIN_CPP_CLIENT_H
 #define MAIN_CPP_CLIENT_H
 
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <cstring>
+
+
+#include <unistd.h>
+
 #endif //MAIN_CPP_CLIENT_H
